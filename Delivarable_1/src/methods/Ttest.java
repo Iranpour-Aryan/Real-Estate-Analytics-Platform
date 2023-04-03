2d18ec8e014db267677123253a3bd6c7dfa9a305
Dang Tran <trmdang@my.yorku.ca>
2023-04-03 14:34:32 -0400

    Delete main() in TTest

--- Delivarable_1/src/methods/Ttest.java
+++ Delivarable_1/src/methods/Ttest.java
@@ -22,21 +22,4 @@ public class Ttest {
 		}
 	}
 
-	public static void main(String[] args) {
-		TTest tt = new TTest();
-		double[] sample1 = {30.1, 30.6, 35.4, 20.9, 36.1};
-		double[] sample2 = {22.6, 23.2, 24.8, 40};
-//		tt.pairedT(sample1, sample2);
-		System.out.println("p equals " + tt.tTest(sample1, sample2));
-		if(tt.tTest(sample1, sample2, 0.5) == false) {
-			System.out.println("We cannot reject the null hypothesis");
-		}
-		else {
-			System.out.println("We can reject the null hypothesis");
-		}
-		System.out.println(Integer.parseInt("01"));
-		
-		System.out.println(Integer.parseInt("01" + "05"));
-	}
-
 }
